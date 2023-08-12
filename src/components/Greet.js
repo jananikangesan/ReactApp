ea529accd4a4468d0500b7303fcbf30fa12fdef4
jananikangesan <janakanges1996@gmail.com>
2023-08-12 20:56:17 +0530

    Functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Greet(){
+
+//     return <h1>Hello Computer</h1>
+// }
+
+const Greet=()=><h1>Hello Science</h1>
+
+//export const Greet=()=><h1>Hello DCS</h1>  //when use the export before const,{Greet} should be import in App.js
+export default Greet;
