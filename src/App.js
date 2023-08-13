f507b98a8df3b63122a1c561522ef41f41fafa74
jananikangesan <janakanges1996@gmail.com>
2023-08-13 22:20:47 +0530

    State

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
 
-      <Greet name='Bruce' heroName="Batman">
+      <Message/>
+
+      {/* <Greet name='Bruce' heroName="Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name='Clark' heroName="Superman">
@@ -21,7 +24,7 @@ function App() {
       <Welcome name='Clark' heroName="Superman"/>
       <Welcome name='Diana' heroName="wounder Women"/>
 
-      <Hello/>
+      <Hello/> */}
      
     </div>
   );
