677224d62bc6b7cd6d33cba6fed544d9a058a138
jananikangesan <janakanges1996@gmail.com>
2023-08-20 17:49:13 +0530

    Binding event Handlers

---
+++ src/components/EventBind.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             message:'Hello'
+        }
+       //this.clickHandler=this.clickHandler.bind(this)  //this is used  for 3rd method
+    }
+
+    // clickHandler(){
+
+    //     this.setState({
+    //         message:'Goodbye!'
+    //     })
+    //     console.log(this)
+    // }
+
+    // for 4th method
+    clickHandler = ()=>{
+        this.setState({
+            message:'Good Bye!'
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+                {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+                {/* <button onClick={()=>this.clickHandler()}>Click</button> */}
+                
+                {/* 3rd method , 4th method*/}
+                <button onClick={this.clickHandler}>Click</button>
+    
+            </div>
+        )
+    }
+}
+
+export default EventBind
