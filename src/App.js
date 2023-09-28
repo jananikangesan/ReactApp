b22ffc57023a070865264e8692033e8221560d13
jananikangesan <janakanges1996@gmail.com>
2023-09-28 19:45:46 +0530

    Pure Components

--- src/App.js
+++ src/App.js
@@ -19,13 +19,17 @@ import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 
 function App() {
   return (
     <div className="App">
 
-    <Table/>
+    <ParentComp/>
+
+    {/* <Table/> */}
     {/* <FragmentDemo/> */}
 
     {/* <LifecycleA/> */}
