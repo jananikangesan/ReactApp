df92ab475f954b98a92a1660823005846fb42017
jananikangesan <janakanges1996@gmail.com>
2023-09-28 19:15:26 +0530

    Fragments

--- src/App.js
+++ src/App.js
@@ -17,15 +17,18 @@ import './appStyles.css';
 import styles from './appStyles.module.css';
 import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 
 function App() {
   return (
     <div className="App">
 
+    <Table/>
+    {/* <FragmentDemo/> */}
 
-
-    <LifecycleA/>
+    {/* <LifecycleA/> */}
 
       {/* <Form/> */}
 
