16e449aab3d5a4e2d52ba9f37d1bb9330e6aa9bc
jananikangesan <janakanges1996@gmail.com>
2023-09-30 19:47:26 +0530

    Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -23,14 +23,16 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 
 function App() {
   return (
     <div className="App">
 
+    <FRParentInput/>
 
-    <FocusInput/>
+    {/* <FocusInput/> */}
       {/* <RefsDemo/> */}
 
     {/* <ParentComp/> */}
